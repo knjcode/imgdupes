3b0676aca40d93a0bf96966b614199645bc823d2
knjcode <knjcode@gmail.com>
2018-05-27 12:41:02 +0900

    Update version 0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="imgdupes",
-    version="0.0.8",
+    version="0.0.9",
     author="Kenji Doi",
     author_email="knjcode@gmail.com",
     description="CLI tool to dedupe images based on perceptual hash",
