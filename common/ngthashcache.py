5421bcf831e8cd0405c09d00858db2d719cebc99
knjcode <knjcode@gmail.com>
2018-05-27 16:21:53 +0900

    Drop whash-db4 option

--- common/ngthashcache.py
+++ common/ngthashcache.py
@@ -83,10 +83,8 @@ class NgtHashCache:
             hashfunc = imagehash.phash
         elif hash_method == 'dhash':
             hashfunc = imagehash.dhash
-        elif hash_method == 'whash-haar':
+        elif hash_method == 'whash':
             hashfunc = imagehash.whash
-        elif hash_method == 'whash-db4':
-            hashfunc = lambda img: imagehash.whash(img, mode='db4')
         return hashfunc
 
 
